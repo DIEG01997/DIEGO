bca1b59e97de36a690d0281822bbee0d8fd7b36f
DIEG01997 <damartinezo01@gmail.com>
2025-10-14 20:32:58 +0200

    14/10/25

---
+++ src/PROGRAMAS/GestionFicheros.java
@@ -0,0 +1,115 @@
+package PROGRAMAS;
+
+//_________________________________________________IMPORTACIÓN DE LIBRERÍAS__________
+
+//Librerías de gestión de ficheros:
+import java.io.*;
+
+//Librerías útiles de java:
+import java.util.*;
+//_____________________________________________________________________________________
+
+public class GestionFicheros {
+
+/*__________________________________________________ENUNCIADO__________________________
+ * 	
+ *                  PROGRAMA DE GESTIÓN DE UN SISTEMA DE ARCHIVOS.
+ * 
+ *  Realiza un programa que permita gestionar un sistema de archivos. En primera instancia
+ *  el programa deberá saludar al usuario y le pedirá al usuario que intruduzca un nombre
+ *  para la carpeta raíz del sistema de ficheros (esta carpeta contendrá todas las carpetas
+ *  y ficheros que se creen durante la ejecución del programa).
+ *  
+ *  A continuación, el programa mostrará el siguiente menú principal:
+ *  
+ *  
+ *  
+ *  					~~~ MENÚ PRINCIPAL ~~~
+ *  				
+ *  					1. Gestionar carpetas.
+ *  					2. Gestionar ficheros.
+ *  					3. Listar directorios.
+ *  					4. Salir.
+ *  
+ *  
+ *  
+ *** OPCIÓN 1: si el usuario selecciona la opción 1, se mostrará el siguiente menú:
+ *  
+ *  					
+ *  					~~~ MENÚ DE GESTIÓN DE CARPETAS ~~~
+ *  
+ *  					1. Crear carpeta.
+ *  					2. Listar carpetas.
+ *  					3. Renombrar carpeta.
+ *  					4. Eliminar carpeta.
+ *  					5. Gestionar permisos de una carpeta.
+ *  					6. Salir.
+ *  
+ *  
+ *  
+ *** OPCIÓN 2: si el usuario selecciona la opción 2 del menú principal, se mostrará el
+ *   siguiente menú:
+ *  
+ *   
+ *   					~~~ MENÚ DE GESTIÓN DE FICHEROS ~~~
+ *   
+ *   					1. Crear fichero.
+ *   					2. Listar ficheros.
+ *   					3. Renombrar Fichero.
+ *   					4. Editar fichero.
+ *   					5. Leer fichero.
+ *   					6. Gestionar permisos de un fichero.
+ *   					7. Eliminar fichero.
+ *   
+ *   
+ *   
+ *** OPCIÓN 3: si el usuario selecciona la opción 3 del menú principal, se mostrará la 
+ *   estructura herárquica del sistema de ficheros generado durante la ejecución del 
+ *   programa.
+ *   										
+ *  
+ *  
+ *** OPCIÓN 4: si el usuario selecciona la opción 4 del menú principal, el programa 
+ *   mostrará el tamaño del sistema de ficheros junto con un mensaje de despedida.  					
+ *  
+ * 
+ * 
+ * 
+ * CONSIDERACIONES:
+ * 
+ * --> Se valorarán funciones extras.
+ * 
+ * --> El listado de carpetas y ficheros debe realizarse por el tamaño que ocupen en memoria.
+ * 
+ * --> Cada carpeta y fichero debe tener una clave única que permita identificarlo de manera
+ * unívoca. Dicha clave será un ID autoincremental que indique el orden de creación del elemento
+ * en el sistema de archivos.
+ * 
+ * --> La carpeta raíz debe ser inmutable en nombre y permisos.
+ * 
+ * --> El listado de directorios debe ser en forma de cascada, notándose la estructura
+ * jerárquica del sistema de archivos.
+ * 
+ * --> Debe poderse leer y escribir datos binarios.
+ * 
+ * --> Debe poderse eliminar información de un fichero, corregir información y añadir.
+ * 
+ * --> Deben poder controlarse todas las posibles excepciones que salten durante la 
+ * ejecución. 
+ * 
+ * --> Preguntar al usuario si desea finalizar con una acción que conlleve la pérdida 
+ * de información en el sistema de ficheros.
+ * 
+ * --> Implementar el método split.
+ * 
+ * 
+ * ___________________________________________________________________________________
+ */
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
